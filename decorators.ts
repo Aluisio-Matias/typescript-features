cf71a51b715e9905a54bf9cf58e2ad79d963ab43
Aluisio-Matias <aluisiomatias1@gmail.com>
2023-07-19 16:03:57 -0700

    more decorators examaples

--- decorators.ts
+++ decorators.ts
@@ -1,20 +1,35 @@
+@classDecorator
 class Boat {
   @testDecorator
   color: string = 'blue';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat color is ${this.color}`;
   }
 
-  @logError('Ooops, boat has sunk in the ocean.')
-  pilot(): void {
-    throw new Error();
-    console.log('swish')
+  @logError('Something bad happened.')
+  pilot(
+    @parameterDecorator speed: string, 
+    @parameterDecorator generateWake: boolean
+    ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing to log');
+    }
   }
 }
 
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor)
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
 function testDecorator(target: any, key: string) {
-  console.log(target.color);
   console.log(key);
 }
 
