05b10adfdbb22105dfcd986a37c161dd1f6fe5d7
Aluisio-Matias <aluisiomatias1@gmail.com>
2023-07-19 22:43:37 -0700

    more metadata examples

--- metadata.ts
+++ metadata.ts
@@ -1,8 +1,38 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'blue'
-};
+@printMetadata
+class Plane {
+  color: string = 'blue';
+
+  @markFunction('Hey there!')
+  fly(): void {
+    console.log('swoooooosh');
+  }
+
+}
+
+function markFunction(secretInfo: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key)
+  }
+}
+
+
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+// console.log(secret)
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret)
+  }
+}
+
+//initial code examples about metadata
+
+// const plane = {
+//   color: 'blue'
+// };
 
 // Reflect.defineMetadata('note', 'Hey there', plane);
 // Reflect.defineMetadata('height', 10, plane);
@@ -13,8 +43,8 @@ const plane = {
 // console.log(note);
 // console.log(height);
 
-Reflect.defineMetadata('note', 'hey there', plane, 'color');
+// Reflect.defineMetadata('note', 'hey there', plane, 'color');
 
-const note = Reflect.getMetadata('note', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
 
-console.log(note);
+// console.log(note);
